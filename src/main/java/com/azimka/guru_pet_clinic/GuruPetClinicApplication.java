1d8ecb8d9fbbe530bcf3f4da587602cb18fd68bc
azimka <aszimka@gmail.com>
2019-01-03 11:43:56 +0300

    Initial commit

---
+++ src/main/java/com/azimka/guru_pet_clinic/GuruPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.azimka.guru_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruPetClinicApplication.class, args);
+	}
+
+}
+
